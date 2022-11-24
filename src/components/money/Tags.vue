df77939378708b39be2f64f3aaeed269a7ad1086
collie <2541474139@qq.com>
2022-11-24 17:46:55 +0800

    写Tags.vue的css

--- src/components/money/Tags.vue
+++ src/components/money/Tags.vue
@@ -1,47 +1,69 @@
 <template>
   <ul class="tags">
     <li>
-      <Icon name="水果" />
+      <div class="tags-icons">
+        <Icon name="水果" />
+      </div>
       <span>水果</span>
     </li>
     <li>
-      <Icon name="餐饮" />
+      <div class="tags-icons">
+        <Icon name="餐饮" />
+      </div>
       <span>餐饮</span>
     </li>
     <li>
-      <Icon name="零食" />
+      <div class="tags-icons">
+        <Icon name="零食" />
+      </div>
       <span>零食</span>
     </li>
     <li>
-      <Icon name="交通" />
+      <div class="tags-icons selected">
+        <Icon name="交通" />
+      </div>
       <span>交通</span>
     </li>
     <li>
-      <Icon name="通讯" />
+      <div class="tags-icons">
+        <Icon name="通讯" />
+      </div>
       <span>通讯</span>
     </li>
     <li>
-      <Icon name="自定义" />
+      <div class="tags-icons">
+        <Icon name="自定义" />
+      </div>
       <span>自定义</span>
     </li>
     <li>
-      <Icon name="自定义" />
+      <div class="tags-icons">
+        <Icon name="自定义" />
+      </div>
       <span>自定义</span>
     </li>
     <li>
-      <Icon name="自定义" />
+      <div class="tags-icons">
+        <Icon name="自定义" />
+      </div>
       <span>自定义</span>
     </li>
     <li>
-      <Icon name="自定义" />
+      <div class="tags-icons">
+        <Icon name="自定义" />
+      </div>
       <span>自定义</span>
     </li>
     <li>
-      <Icon name="自定义" />
+      <div class="tags-icons">
+        <Icon name="自定义" />
+      </div>
       <span>自定义</span>
     </li>
     <li>
-      <Icon name="自定义" />
+      <div class="tags-icons">
+        <Icon name="自定义" />
+      </div>
       <span>自定义</span>
     </li>
   </ul>
@@ -55,13 +77,36 @@ export default class Tags extends Vue {}
 </script>
 
 <style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
 .tags {
   display: flex;
+  flex-direction: row;
   flex-wrap: wrap;
+  overflow: auto;
   border-top: 1px solid #eee;
   > li {
     display: flex;
     flex-direction: column;
+    width: 25%;
+    justify-content: center;
+    align-items: center;
+    padding: 8px 0;
+    font-size: 14px;
+    > .tags-icons {
+      width: 48px;
+      height: 48px;
+      padding: 4px;
+      border-radius: 50%;
+      background: rgb(221, 221, 221);
+      margin-bottom: 4px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      &.selected {
+        background: $color-highlight;
+        color: #fff;
+      }
+    }
   }
   ::v-deep .icon {
     width: 30px;
