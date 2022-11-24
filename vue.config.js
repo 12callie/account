02d475bf47f224decbc025c83f6d0b89e44f47d1
collie <2541474139@qq.com>
2022-11-24 23:00:30 +0800

    使用svgo-loader

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,40 @@
-const path = require('path');
+
+const path = require("path");
 
 module.exports = {
   lintOnSave: false,
-  chainWebpack: config => {
-    const dir = path.resolve(__dirname, 'src/assets/icons');
+  chainWebpack: (config) => {
+    const dir = path.resolve(__dirname, "src/assets/icons");
+
     config.module
-      .rule('svg-sprite')
+      .rule("svg-sprite")
       .test(/\.svg$/)
-      .include.add(dir).end()
-      .use('svg-sprite-loader').loader('svg-sprite-loader').options({ extract: false }).end();
-    config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{ plainSprite: true }]);
-    config.module.rule('svg').exclude.add(dir);
-  }
+      .include.add(dir)
+      .end()
+      .use("svg-sprite-loader")
+      .loader("svg-sprite-loader")
+      .options({ extract: false })
+      .end()
+      .before('svg-sprite-loader')
+      .use('svgo-loader')
+      .loader('svgo-loader')
+      .options({
+        plugins: [
+          //!!!!!!!!!!!!!!!重点就是改这个位置，加个插件名字
+          {
+            name: 'removeAttrs',
+            params: {
+              attrs: '(fill|stroke)'
+            }
+          }
+        ]
+      })
+      .end();
+    config
+      .plugin("svg-sprite")
+      .use(require("svg-sprite-loader/plugin"), [{ plainSprite: true }]);
+    config.module.rule("svg").exclude.add(dir);
+
+  },
+
 };
